24d8a145380d4d3a62b2559056f8456fce781b01
virtualarchitectures <oliver.dawkins.13@ucl.ac.uk>
2018-09-14 12:10:47 +0100

    Support for TMPro and Standard Unity text elements

--- DateTime4UI/DateTime4UI.cs
+++ DateTime4UI/DateTime4UI.cs
@@ -3,37 +3,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro; //Required if using TMPro for UI elements
+
+//Comment this code if not using Text Mesh Pro 
+using TMPro;
 
 public class DateTime4UI : MonoBehaviour {
 
     //Variable references for UI elements
     public GameObject hourUI;
     public GameObject dateUI;
+
+    //Comment this code if using regular Unity UI text elements
     private TMP_Text hourText;
     private TMP_Text dateText;
 
-	void Start () {
-        //Initialise the UI elements
-        hourUI = GameObject.Find("Time");
-        dateUI = GameObject.Find("Date");
 
-        //(Un)comment this code for use with TMPro
+    void Start ()
+    {
+        //Comment this code if using regular Unity UI text elements
         hourText = hourUI.GetComponent<TextMeshProUGUI>();
-        dateText = dateUI.GetComponent<TextMeshProUGUI>();
-        
-        //OR
-        
-        //(Un)comment this code for use with regular Unity UI text elements
-        //hourText = hourUI.GetComponent<Text>();
-        //dateText = dateUI.GetComponent<Text>();
+        dateText = dateUI.GetComponent<TextMeshProUGUI>();        
     }
 	
-	void Update () {
+	void Update ()
+    {
         //Get the current date and time
         DateTime dtValue = DateTime.Now;
+
         //Set date time format and pass to UI 
+
+        //Comment this code if using regular Unity UI text elements
         hourText.SetText(dtValue.ToString("h:mm tt").ToLower());
         dateText.SetText(dtValue.ToString("dddd, d MMMM, yyyy"));
+
+        //Uncomment this code for use with regular Unity UI text elements
+        //hourUI.GetComponent<Text>().text = dtValue.ToString("h:mm tt").ToLower();
+        //dateUI.GetComponent<Text>().text = dtValue.ToString("dddd, d MMMM, yyyy");
     }
 }
